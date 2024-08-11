91793513b3594e48883cdaf5e7f33f87ca7c8c57
Yaawar-Askari <f20220787@pilani.bits-pilani.ac.in>
2024-08-11 21:01:54 +0530

    Initial commit of modified CCVAE code for NLP with Trustpilot dataset

--- data_loader.py
+++ data_loader.py
@@ -1,54 +1,68 @@
 import random
 import numpy as np
 from itertools import cycle
-from utils import imshow_grid, transform_config
-
-from torchvision import datasets
 from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer
+import pandas as pd
 
 
-class MNIST_Paired(Dataset):
-    def __init__(self, root='mnist', download=True, train=True, transform=transform_config):
-        self.mnist = datasets.MNIST(root=root, download=download, train=train, transform=transform)
+class TrustpilotPaired(Dataset):
+    def __init__(self, csv_file, tokenizer, max_length=128, train=True, transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.train = train
+        self.transform = transform
+
+        # Split the data into training and validation sets
+        train_data, val_data = train_test_split(self.data, test_size=0.2, random_state=42)
+        self.data = train_data if train else val_data
 
         self.data_dict = {}
 
-        for i in range(self.__len__()):
-            image, label = self.mnist.__getitem__(i)
+        for i in range(len(self.data)):
+            review = self.data.iloc[i]['review']
+            label = self.data.iloc[i]['sentiment']
 
-            try:
-                self.data_dict[label.item()]
-            except KeyError:
-                self.data_dict[label.item()] = []
-            self.data_dict[label.item()].append(image)
+            if label not in self.data_dict:
+                self.data_dict[label] = []
+            self.data_dict[label].append(review)
 
     def __len__(self):
-        return self.mnist.__len__()
+        return len(self.data)
 
     def __getitem__(self, index):
-        image, label = self.mnist.__getitem__(index)
+        review = self.data.iloc[index]['review']
+        label = self.data.iloc[index]['sentiment']
 
-        # return another image of the same class randomly selected from the data dictionary
-        # this is done to simulate pair-wise labeling of data
-        return image, random.SystemRandom().choice(self.data_dict[label.item()]), label
+        # Tokenize the review
+        inputs = self.tokenizer(review, max_length=self.max_length, padding='max_length', truncation=True, return_tensors="pt")
+        input_ids = inputs['input_ids'].squeeze()
+        attention_mask = inputs['attention_mask'].squeeze()
+
+        # Randomly select another review of the same sentiment
+        paired_review = random.SystemRandom().choice(self.data_dict[label])
+        paired_inputs = self.tokenizer(paired_review, max_length=self.max_length, padding='max_length', truncation=True, return_tensors="pt")
+        paired_input_ids = paired_inputs['input_ids'].squeeze()
+        paired_attention_mask = paired_inputs['attention_mask'].squeeze()
+
+        return input_ids, attention_mask, paired_input_ids, paired_attention_mask, label
 
 
 if __name__ == '__main__':
     """
-    test code for data loader
+    Test code for the data loader
     """
-    mnist_paired = MNIST_Paired()
-    loader = cycle(DataLoader(mnist_paired, batch_size=16, shuffle=True, num_workers=0, drop_last=True))
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    trustpilot_paired = TrustpilotPaired(csv_file='path/to/trustpilot.csv', tokenizer=tokenizer)
+    loader = cycle(DataLoader(trustpilot_paired, batch_size=16, shuffle=True, num_workers=0, drop_last=True))
 
-    print(mnist_paired.data_dict.keys())
+    print(trustpilot_paired.data_dict.keys())
 
-    image_batch, image_batch_2, labels_batch = next(loader)
+    input_ids_batch, attention_mask_batch, paired_input_ids_batch, paired_attention_mask_batch, labels_batch = next(loader)
     print(labels_batch)
 
-    image_batch = np.transpose(image_batch, (0, 2, 3, 1))
-    image_batch = np.concatenate((image_batch, image_batch, image_batch), axis=3)
-    imshow_grid(image_batch)
-
-    image_batch_2 = np.transpose(image_batch_2, (0, 2, 3, 1))
-    image_batch_2 = np.concatenate((image_batch_2, image_batch_2, image_batch_2), axis=3)
-    imshow_grid(image_batch_2)
+    # Since this is text data, visualization would typically involve text processing or inspection
+    print(tokenizer.decode(input_ids_batch[0]))
+    print(tokenizer.decode(paired_input_ids_batch[0]))
